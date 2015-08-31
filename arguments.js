c647baad5bd7903304d878008a3c79a63732ecea
oscar + jake <oscar + jake@aa.io>
2015-08-31 11:19:24 -0400

    write myBind

--- arguments.js
+++ arguments.js
@@ -7,3 +7,18 @@ var sum = function(){
 
   return args.reduce(twosum)
 }
+
+
+
+Function.prototype.myBind = function(context){
+  fn = this
+
+  var outerArgs = Array.prototype.slice.call(arguments, 1)
+
+  return function(){
+    var innerArgs = Array.prototype.slice.call(arguments);
+    var allArgs = outerArgs.concat(innerArgs);
+    return fn.apply(context, allArgs)
+  };
+
+};
