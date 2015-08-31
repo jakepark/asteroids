c2fe30853d105a8151f0d20f919301f26859ccff
oscar + jake <oscar + jake@aa.io>
2015-08-31 15:31:45 -0400

    implement gameView

---
+++ asteroids/lib/gameView.js
@@ -0,0 +1,17 @@
+(function (){
+  window.Asteroids = window.Asteroids || {};
+
+  var GameView = Asteroids.GameView = function(game, ctx) {
+    this.game = game;
+    this.ctx = ctx;
+  }
+
+  GameView.prototype.start = function () {
+    var view = this;
+    window.setInterval(function (){
+      view.game.draw(view.ctx);
+      view.game.moveObjects();
+    }, 20);
+  }
+
+})();
