ce6cb2800caa7eb1bc3cfc72acd1c5baf0e3fa74
oscar + jake <oscar + jake@aa.io>
2015-08-31 11:54:28 -0400

    write Function#inherits

--- inherits.js
+++ inherits.js
@@ -1,28 +1,3 @@
-function Animal (name) {
-  this.name = name;
-};
-
-Animal.prototype.sayHello = function () {
-  console.log("Hello, my name is " + this.name);
-};
-
-function Dog () {};
-
-// The surrogate will be used to construct `Dog.prototype`.
-function Surrogate () {};
-// A `Surrogate` instance should delegate to `Animal.prototype`.
-Surrogate.prototype = Animal.prototype;
-
-// Set `Dog.prototype` to a `Surrogate` instance.
-// `Surrogate.__proto__` is `Animal.prototype`, but `new
-// Surrogate` does not invoke the `Animal` constructor function.
-Dog.prototype = new Surrogate();
-
-Dog.prototype.bark = function () {
-  console.log("Bark!");
-};
-
-
 Function.prototype.inherits = function(parentClass) {
   // childClass = this;
   function Surrogate () {};
