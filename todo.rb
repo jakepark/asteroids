8943136350c548268e1682570d923fdd76aa12a3
jake park <jake.park@caa.columbia.edu>
2015-10-03 15:52:09 -0400

    Update todo, enemies

--- todo.rb
+++ todo.rb
@@ -1,6 +1,14 @@
 1. Set timer for ship respawn  DONE
-2. Respawn enemies DONE
+2. A : Respawn enemies DONE
+2. B : Asteroids spawn RANDOM RADII DONE
+2. C : Sprites scale to the hitboxes DONE
 3. Asteroid Collisions
+
+  V = (4/3) pi r^3
+  d = m / V .... assume d = 1 kg / m^3;
+  V ~= m
+
+  m1v1 + m2v2 = m1*v1* + m2*v2*
 4. Ship velocity
 5. input multiple keys
 6. Display overlay of ship statistics
